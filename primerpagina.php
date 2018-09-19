c065871044e0a067444d3dc129b62b4f7f21c0ea
Norlan Madrigal <madrigalnorlan8@gmail.com>
2018-09-18 20:52:25 -0600

    primera practica en clase

---
+++ primerpagina.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi Primera pagina con PHP</title>
+</head>
+<body>
+	<img src="https://media.giphy.com/media/pLiyRPqPyxy7e/source.gif">
+	<?php echo "<h1>Mi pagina Web con PHP</h1>"; ?>
+</body>
+</html>
